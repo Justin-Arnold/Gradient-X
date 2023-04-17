676a412fd30c78ef2f5d6f64f186d2e8f97b1beb
Justin Arnold <justin.arnold@programmer.net>
2023-04-16 22:12:49 -0400

    update login method to redirect after login

--- stores/session.ts
+++ stores/session.ts
@@ -21,26 +21,24 @@ export const useSessionStore = defineStore('session', {
             this.isAuthenticated = false
             navigateTo('/')
         },
-        async login() {
-            const searchParams = new URLSearchParams(location.search);
+        async login(email: string, password: string) {
             const resp = $fetch('http://localhost:3000/api3/login', {
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 method: 'POST',
                 body: JSON.stringify({
-                    username: 'admin@school.com',
-                    password: 'password',
+                    username: email,
+                    password: password,
                     generateToken: false,
                     redirect: null,
                     extAuth: false,
                 }),
             });
-            console.log('resp', resp)
             const data: any = await resp
-            console.log('data', data)
             this.isAuthenticated = true
             this.userId = data._id || null
+            navigateTo('/home')
         },
     },
     persist: true
