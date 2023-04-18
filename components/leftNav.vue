d8389c6b5a2036fc9b301c0523ed8f9941e18edf
Justin Arnold <justin.arnold@programmer.net>
2023-04-17 21:56:04 -0400

    update left nav visuals

--- components/leftNav.vue
+++ components/leftNav.vue
@@ -1,29 +1,29 @@
 <template>
-    <div class="bg-gray-500 h-full w-fit flex flex-col items-center shadow-md shadow-neutral-600 z-[999]">
-            <div class="mt-1 px-1">
-                <nuxt-img src="https://gcdnb.pbrd.co/images/t2Dd3ktfvy1R.png?o=1" class="rounded aspect-square w-12"/>
+    <div class="bg-blue-500 p-2 h-full w-16 flex flex-col items-center shadow-md shadow-neutral-600 z-[999]">
+        <nuxt-img
+            src="https://gcdnb.pbrd.co/images/t2Dd3ktfvy1R.png?o=1"
+            class="rounded aspect-square shadow shadow-blue-800"
+        />
+        <nav class="flex flex-col w-full gap-2 mt-12">
+            <NuxtLink
+                v-for="each, index in model"
+                :key="index"
+                :to="each.route"
+                class="text-white grid place-items-center w-full aspect-square"
+                active-class="bg-slate-200/30 rounded-xl"
+            >
+                <Icon :name="each.icon" size="28px"></Icon>
+            </NuxtLink>
+        </nav>
+        <div class="flex flex-col grow">
+            <div class="grow"></div>
+            <div
+                title="Log Out"
+                @click="logout"
+                class="cursor-pointer bg-white text-stone-500 text-lg text-light aspect-square px-1 grid place-items-center rounded-full mb-2">
+                JA
             </div>
-            <nav class="flex flex-col gap-4 mt-8">
-                <NuxtLink
-                    v-for="each, index in model"
-                    :key="index"
-                    :to="each.route"
-                    class="text-white grid place-items-center text-[8px] px-1"
-                    active-class="bg-white !text-blue-500"
-                >
-                    <Icon :name="each.icon" size="24px"></Icon>
-                    {{ each.name }}
-                </NuxtLink>
-            </nav>
-            <div class="flex flex-col grow">
-                <div class="grow"></div>
-                <div
-                    title="Log Out"
-                    @click="logout"
-                    class="cursor-pointer bg-white text-stone-500 text-lg text-light aspect-square px-1 grid place-items-center rounded-full mb-2">
-                    JA
-                </div>
-            </div>
+        </div>
     </div>
 </template>
 
