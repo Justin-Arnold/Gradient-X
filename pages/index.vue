191d011dbf21027745dbe015d31ff078c7dc1198
Justin Arnold <justin.arnold@programmer.net>
2023-04-19 13:46:35 -0400

    update session store

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,11 @@ import { useSessionStore } from '~~/stores/session'
 import { storeToRefs } from 'pinia';
 
 const sessionStore = useSessionStore()
-const { lastLoginType, isAuthenticated } = storeToRefs(sessionStore)
+const { isAuthenticated } = storeToRefs(sessionStore)
 
 onBeforeMount(() => {
     if (isAuthenticated.value) {
         navigateTo('/home')
-    } else if (lastLoginType.value === 'student') {
-        navigateTo('/loginStudent')
     } else {
         navigateTo('/login')
     }
--- stores/session.ts
+++ stores/session.ts
@@ -8,48 +8,48 @@ interface State {
     userId: string | null,
 }
 
-const userStore = useUserStore();
-const { userId } = storeToRefs(userStore);
 
-export const useSessionStore = defineStore('session', {
-    state: (): State => {
-        return {
-            lastLoginType: null,
-            isAuthenticated: false,
-            userId: null,
+
+export const useSessionStore = defineStore('session', () => {
+
+    const userStore = useUserStore();
+    const { userId } = storeToRefs(userStore);
+
+    const isAuthenticated = ref(false)
+
+    function logout() {
+        isAuthenticated.value = false
+        navigateTo('/')
+    }
+    async function login(email: string, password: string, student?: boolean) {
+        if (student) {
+            navigateTo('/student-portal')
         }
-    },
-    actions: {
-        logout() {
-            this.lastLoginType = null
-            this.isAuthenticated = false
-            navigateTo('/')
-        },
-        async login(email: string, password: string, student?: boolean) {
-            if (student) {
-                navigateTo('/student-portal')
+        const resp = await useFetch('/api3/login', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                username: email,
+                password: password,
+                generateToken: false,
+                redirect: null,
+                extAuth: false,
+            }),
+            params: {
+                gcToken: '',
             }
-            const resp = await useFetch('/api3/login', {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                method: 'POST',
-                body: JSON.stringify({
-                    username: email,
-                    password: password,
-                    generateToken: false,
-                    redirect: null,
-                    extAuth: false,
-                }),
-                params: {
-                    gcToken: '',
-                }
-            });
-            const data: any = await resp
-            this.isAuthenticated = true
-            userId.value = data._id || null
-            navigateTo('/home')
-        },
-    },
-    persist: true
+        });
+        const data: any = await resp
+        isAuthenticated.value = true
+        userId.value = data._id || null
+        navigateTo('/home')
+    }
+
+    return {
+        isAuthenticated,
+        logout,
+        login,
+    }
 })
